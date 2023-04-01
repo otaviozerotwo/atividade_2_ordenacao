9d08eb759fea8c1b902fbb827b893acfb2c322c0
otaviozerotwo <deadplatypus@protonmail.com>
2023-03-31 21:27:17 -0300

    implementação bubble_sort

--- ordenacao.c
+++ ordenacao.c
@@ -1,46 +1,66 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void menu(){
-    int opcao = 0;
+#define MAX_LINE_LENGTH 1024
+#define MAX_NUM_LINES 1000
 
-    printf("Selecione o método de ordenação:\n");
-    printf("1 - Bubble Sort\n"
-           "2 - Selection Sort\n"
-           "3 - Insertion Sort\n");
-
-    scanf("%d", &opcao);
-
-    switch(opcao){
-    case 1:
-        bubble_sort();
-        break;
-    case 2:
-        selection_sort();
-        break;
-    case 3:
-        insertion_sort();
-        break;
-    default:
-        printf("Opção inválida!");
-        break;
+void bubble_sort(char **array, int size) {
+    int i, j;
+    char *tmp;
+    for (i = 0; i < size - 1; i++) {
+        for (j = 0; j < size - i - 1; j++) {
+            if (strcmp(array[j], array[j+1]) > 0) {
+                tmp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = tmp;
+            }
+        }
     }
 }
 
-void bubble_sort(){
+int main() {
+    FILE *fp = fopen("dados.txt", "r");
+    if (fp == NULL) {
+        printf("Error: failed to open input file\n");
+        return 1;
+    }
 
-}
+    char **lines = malloc(sizeof(char*) * MAX_NUM_LINES);
+    int num_lines = 0;
+    char buffer[MAX_LINE_LENGTH];
+    while (fgets(buffer, MAX_LINE_LENGTH, fp) != NULL) {
+        int len = strlen(buffer);
+        if (len > 0 && buffer[len-1] == '\n') {
+            buffer[len-1] = '\0';
+        }
+        lines[num_lines] = strdup(buffer);
+        num_lines++;
+        if (num_lines >= MAX_NUM_LINES) {
+            printf("Error: input file has too many lines (max: %d)\n", MAX_NUM_LINES);
+            return 1;
+        }
+    }
+    fclose(fp);
 
-void selection_sort(){
+    bubble_sort(lines, num_lines);
 
-}
+    char **matrix = malloc(sizeof(char*) * num_lines);
+    for (int i = 0; i < num_lines; i++) {
+        matrix[i] = lines[i];
+    }
 
-void insertion_sort(){
+    printf("Sorted matrix:\n");
+    for (int i = 0; i < num_lines; i++) {
+        printf("%s\n", matrix[i]);
+    }
 
-}
+    for (int i = 0; i < num_lines; i++) {
+        free(lines[i]);
+    }
+    free(lines);
+    free(matrix);
 
-int main(){
-    //Chama menu de métodos de ordenação
-    menu();
-
+    printf("Done!\n");
     return 0;
 }
