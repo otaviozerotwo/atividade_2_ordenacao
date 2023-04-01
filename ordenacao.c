8e05b866e3bd7748fd6da8c8514c41cd8d18b608
otaviozerotwo <deadplatypus@protonmail.com>
2023-04-01 13:24:27 -0300

    definição struct para armazenar conteúdo do arquivo

--- ordenacao.c
+++ ordenacao.c
@@ -1,114 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-void bubbleSort(char *arr[], int n);
-void selectionSort(char *arr[], int n);
-void insertionSort(char *arr[], int n);
+typedef struct{
+    char nome_time[100];
+    char nome_adversario[100];
+    char resultado[100];
+    //int vitorias;
+    //int derrotas;
+    //int empates;
+    //int pontos;
+} Partida;
 
-int main()
-{
-    char **arr;
-    int n;
-    int escolha;
 
-    FILE *arquivo = fopen("dados.txt", "r");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
+int main(){
+    Partida partidas[100];
+
+    FILE* arquivo = fopen("dados.txt", "r");
+    if(arquivo == NULL){
+        printf("Erro ao abrir o arquivo!\n");
         exit(1);
     }
 
-    fscanf(arquivo, "%d", &n);
-
-    arr = (char**) malloc(n * sizeof(char*));
-    if (arr == NULL) {
-        printf("Erro ao alocar a memória.\n");
-        exit(1);
-    }
-
-    for (int i = 0; i < n; i++) {
-        char buffer[256];
-        fscanf(arquivo, "%s", buffer);
-        arr[i] = (char*) malloc((strlen(buffer) + 1) * sizeof(char));
-        if (arr[i] == NULL) {
-            printf("Erro ao alocar a memória.\n");
-            exit(1);
-        }
-        strcpy(arr[i], buffer);
+    int i = 0;
+    while(arquivo != EOF){
+        char linha[100];
+        fgets(linha, 100, arquivo);
+        char* time = strtok(linha, " ");
+        char* resultado = strtok(NULL, " ");
+        char* adversario = strtok(NULL, " ");
+        printf("%s\n", strcpy(partidas[i].nome_time, time));
+        printf("%s\n", strcpy(partidas[i].resultado, resultado));
+        printf("%s\n", strcpy(partidas[i].nome_adversario, adversario));
+        i++;
     }
 
     fclose(arquivo);
-
-    printf("Escolha um algoritmo de ordenação:\n");
-    printf("1 - Bubble Sort\n");
-    printf("2 - Selection Sort\n");
-    printf("3 - Insertion Sort\n");
-    scanf("%d", &escolha);
-
-    switch (escolha) {
-        case 1:
-            bubbleSort(arr, n);
-            break;
-        case 2:
-            selectionSort(arr, n);
-            break;
-        case 3:
-            insertionSort(arr, n);
-            break;
-        default:
-            printf("Escolha inválida.\n");
-            exit(1);
+/*
+    for(int j = 0; j < i; j++){
+        printf("%s\n%s\n%s\n", partidas[j].nome_time, partidas[j].resultado, partidas[j].nome_adversario);
     }
-
-    printf("Array ordenado:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%s\n", arr[i]);
-    }
-
-    for (int i = 0; i < n; i++) {
-        free(arr[i]);
-    }
-    free(arr);
+*/
     return 0;
-}
-
-void bubbleSort(char *arr[], int n)
-{
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (strcmp(arr[j], arr[j + 1]) > 0) {
-                char *temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
-
-void selectionSort(char *arr[], int n)
-{
-    for (int i = 0; i < n - 1; i++) {
-        int min_idx = i;
-        for (int j = i + 1; j < n; j++) {
-            if (strcmp(arr[j], arr[min_idx]) < 0) {
-                min_idx = j;
-            }
-        }
-        char *temp = arr[i];
-        arr[i] = arr[min_idx];
-        arr[min_idx] = temp;
-    }
-}
-
-void insertionSort(char *arr[], int n)
-{
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = key;
-    }
 }
