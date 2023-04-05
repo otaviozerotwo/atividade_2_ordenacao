dba50bfc457b8cd0155704256466b5863efc5910
otaviozerotwo <deadplatypus@protonmail.com>
2023-04-05 20:55:00 -0300

    implementação selection_sort e ajuste nomes variáveis

--- ordenacao.c
+++ ordenacao.c
@@ -14,9 +14,9 @@ typedef struct{
     int saldo_gols;
     int gols_pro;
     int gols_contra;
-} Partida;
+} Time;
 
-void imprimir_tabela(Partida* partidas, int* num_times){
+void imprimir_tabela(Time* time, int* num_times){
     printf("TIME\t");
     printf("PONTOS\t");
     printf("VITORIAS\t");
@@ -27,18 +27,18 @@ void imprimir_tabela(Partida* partidas, int* num_times){
     printf("GOLS_CONTRA\n");
 
     for(int j = 0; j < num_times; j++){
-            printf("%s\t", partidas[j].nome_time);
-            printf("  %d\t", partidas[j].pontos);
-            printf("  %d\t\t", partidas[j].vitorias);
-            printf("  %d\t", partidas[j].empates);
-            printf("  %d\t\t", partidas[j].derrotas);
-            printf("  %d\t", partidas[j].saldo_gols);
-            printf("  %d\t\t", partidas[j].gols_pro);
-            printf("  %d\n", partidas[j].gols_contra);
+            printf("%s\t", time[j].nome_time);
+            printf("  %d\t", time[j].pontos);
+            printf("  %d\t\t", time[j].vitorias);
+            printf("  %d\t", time[j].empates);
+            printf("  %d\t\t", time[j].derrotas);
+            printf("  %d\t", time[j].saldo_gols);
+            printf("  %d\t\t", time[j].gols_pro);
+            printf("  %d\n", time[j].gols_contra);
     }
 }
 
-int ler_arquivo(char* nome_arquivo, Partida* partidas, int* num_times) {
+int ler_arquivo(char* nome_arquivo, Time* time, int* num_times) {
     FILE* arquivo = fopen(nome_arquivo, "r");
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo.\n");
@@ -53,40 +53,46 @@ int ler_arquivo(char* nome_arquivo, Partida* partidas, int* num_times) {
         sscanf(linha_arquivo, "%s %dX%d %s", time_1, &gols_pro, &gols_contra, time_2);
         int time_1_existe = -1, time_2_existe = -1;
         for (int i = 0; i < *num_times; i++) {
-            if (strcmp(partidas[i].nome_time, time_1) == 0) {
+            if (strcmp(time[i].nome_time, time_1) == 0) {
                 time_1_existe = i;
             }
-            if (strcmp(partidas[i].nome_time, time_2) == 0) {
+            if (strcmp(time[i].nome_time, time_2) == 0) {
                 time_2_existe = i;
             }
         }
         if (time_1_existe == -1) {
-            strcpy(partidas[*num_times].nome_time, time_1);
-            partidas[*num_times].gols_pro = 0;
-            partidas[*num_times].gols_contra = 0;
+            strcpy(time[*num_times].nome_time, time_1);
+            time[*num_times].gols_pro = 0;
+            time[*num_times].gols_contra = 0;
             time_1_existe = (*num_times)++;
         }
         if (time_2_existe == -1) {
-            strcpy(partidas[*num_times].nome_time, time_2);
-            partidas[*num_times].gols_pro = 0;
-            partidas[*num_times].gols_contra = 0;
+            strcpy(time[*num_times].nome_time, time_2);
+            time[*num_times].gols_pro = 0;
+            time[*num_times].gols_contra = 0;
             time_2_existe = (*num_times)++;
         }
-        partidas[time_1_existe].gols_pro += gols_pro;
-        partidas[time_1_existe].gols_contra += gols_contra;
+        time[time_1_existe].gols_pro += gols_pro;
+        time[time_1_existe].gols_contra += gols_contra;
 
         for(int i = 0; i < *num_times; i++){
-            partidas[i].saldo_gols = partidas[i].gols_pro - partidas[i].gols_contra;
+            time[i].saldo_gols = time[i].gols_pro - time[i].gols_contra;
         }
 
-        if(partidas[time_1_existe].gols_pro > partidas[time_2_existe].gols_contra){
-            partidas[time_1_existe].vitorias++;
-            partidas[time_1_existe].pontos += 3;
-        }
-
-        if(partidas[time_2_existe].gols_contra > partidas[time_1_existe].gols_pro){
-            partidas[time_2_existe].vitorias++;
-            partidas[time_2_existe].pontos += 3;
+        if(time[time_1_existe].gols_pro > time[time_2_existe].gols_contra){
+            time[time_1_existe].vitorias++;
+            time[time_1_existe].pontos += 3;
+            time[time_2_existe].derrotas++;
+        }else if(time[time_2_existe].gols_contra > time[time_1_existe].gols_pro){
+            time[time_2_existe].vitorias++;
+            time[time_2_existe].pontos += 3;
+            time[time_1_existe].derrotas++;
+        }else if(time[time_1_existe].gols_pro == time[time_2_existe].gols_contra){
+            time[time_1_existe].empates++;
+            time[time_1_existe].pontos += 1;
+        }else if(time[time_2_existe].gols_contra == time[time_1_existe].gols_pro){
+            time[time_2_existe].empates++;
+            time[time_2_existe].pontos += 1;
         }
 
     }
@@ -96,8 +102,8 @@ int ler_arquivo(char* nome_arquivo, Partida* partidas, int* num_times) {
     return 1;
 }
 
-void bubble_sort(Partida *arr, int n){
-    Partida temp;
+void bubble_sort(Time *arr, int n){
+    Time temp;
     for(int i = 0; i < n - 1; i++){
         for(int j = 0; j < n - i - 1; j++){
             if(arr[j].pontos < arr[j+1].pontos){
@@ -122,25 +128,49 @@ void bubble_sort(Partida *arr, int n){
     }
 }
 
-void selection_sort(Partida *arr, int n) {
+void selection_sort(Time *arr, int n) {
+    Time temp;
+    for(int i = 0; i < n - 1; i++){
+        for(int j = i + 1; j < n; j++){
+            if(arr[j].pontos < arr[j+1].pontos){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }else if(arr[j].pontos == arr[j+1].pontos){
+                if(arr[j].vitorias > arr[j+1].vitorias){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }else if(arr[j].vitorias == arr[j+1].vitorias){
+                    if(arr[j].saldo_gols > arr[j+1].saldo_gols){
+                        temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+
+            }
+        }
+    }
 
 }
 
-void insertion_sort(Partida *arr, int n){
+void insertion_sort(Time *arr, int n){
 
 }
 
 int main(){
     char nome_arquivo[] = "dados.txt";
-    Partida partidas[MAX_TEAMS];
+    Time time[MAX_TEAMS];
     int num_times = 0;
 
-    ler_arquivo(nome_arquivo, partidas, &num_times);
-    //imprimir_tabela(partidas, num_times);
-    //bubble_sort(partidas, num_times);
+    ler_arquivo(nome_arquivo, time, &num_times);
+    imprimir_tabela(time, num_times);
+    //bubble_sort(time, num_times);
+    selection_sort(time, num_times);
 
-    //printf("\n\n");
-    imprimir_tabela(partidas, num_times);
+    printf("\n\n");
+    imprimir_tabela(time, num_times);
 
     return 0;
 }
